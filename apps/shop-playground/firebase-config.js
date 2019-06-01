3ef0fd3e7b6d125a37970fb5ef3a5d3f2d45f1e5
Alex Grafe <xanderiel@xanderia.org>
2019-06-01 22:27:43 +0200

    remove config #5

--- apps/shop-playground/firebase-config.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-	apiKey:				"===",
-	authDomain:			"xanderia-e7b8f.firebaseapp.com",
-	databaseURL:		"https://xanderia-e7b8f.firebaseio.com",
-	projectId:			"xanderia-e7b8f",
-	storageBucket:		"xanderia-e7b8f.appspot.com",
-	messagingSenderId:	"815820095221"
-}
